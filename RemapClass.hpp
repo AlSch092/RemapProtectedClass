fa3d1281146c328819517905078faea5ef344015
Alex S. <94417808+AlSch092@users.noreply.github.com>
2024-10-18 18:19:44 -0400

    Update RemapClass.hpp

--- RemapClass.hpp
+++ RemapClass.hpp
@@ -1,5 +1,7 @@
 // RemapClass.hpp : By AlSch092 @ Github
 // Thanks to changeofpace @ Github for original self-remapping-code example
 
-#include <iostream>
 #include "ntdll.hpp"
+#include <Windows.h>
+#include <cstdio>
+#include <stdexcept>
